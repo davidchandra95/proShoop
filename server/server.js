c5f884cde749dd2eff313b7ed874b62d791be676
davidchandra95 <david.chandra@tokopedia.com>
2020-12-27 23:44:16 +0700

    add custom error

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import colors from 'colors';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middlewares/errorMiddlewares.js';
 
 dotenv.config();
 
@@ -11,6 +12,9 @@ connectDB();
 const app = express();
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
